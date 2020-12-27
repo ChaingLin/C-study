12197f87d0874e94de3b1304944b21f1a55cc3f4
Diwhdkgxgx <jlj1144247539@outlook.com>
2020-12-27 15:50:01 +0800

    输入10个整数,将其中最小的数与第一个数对换,把最大的数与最后一个数对换

--- homework/chapter8/8.3.c
+++ homework/chapter8/8.3.c
@@ -1,18 +1,18 @@
 // 输入10个整数,将其中最小的数与第一个数对换,把最大的数与最后一个数对换。要求用3个函数实现,分别为输入10个数、进行处理、输出10个数。要求使用指针的方法进行处理。
- 跑不出来的一版
- #include<stdio.h>
+
+#include <stdio.h>
 
 int i;
 int main()
 {
-    int a[10];
-    void input(int *num);
-    void output(int *num);
-    void process(int *num);
+    int num[10];
+    void input(int *);
+    void output(int *);
+    void process(int *);
 
-    input(a);
-    process(a);
-    output(a);
+    input(num);
+    process(num);
+    output(num);
     return 0;
 }
 
@@ -23,39 +23,54 @@ void input(int *num)
     {
         scanf("%d", &num[i]);
     }
+    printf("你输入的数是：\n");
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", num[i]);
+    }
+    putchar('\n');
 }
 
 void output(int *num)
 {
-    for ( i = 0; i < 10; i++)
+    printf("结果是：\n");
+    for (i = 0; i < 10; ++i)
     {
-        printf("%d ", num[i]);
+        if (i == 9) // 格式控制
+        {
+            printf("%d\n", num[i]);
+        }        
+        else
+        {
+            printf("%d ", num[i]);
+        }
     }
 }
-
 void process(int *num)
 {
-    int min, max;
-    min = max = num[0];
+    int *min, *max;
+    min = max = &num[0]; // min和max两个指针，指向num的第一个元素的地址
     int k, l, t;
     for (i = 0; i < 10; i++)
     {
-        if (max <= num[i])
+        if (*max <= num[i]) // max指针所指向的数*max，来和num数组的各项比较
         {
-            max = num[i];
+            max = &num[i]; // 指针max，指向当前数组中最大数的地址
             k = i;
         }
-        if (min >= num[i])
+        if (*min >= num[i])
         {
-            min = num[i];
+            min = &num[i];
             l = i;
         }
     }
     t = num[0];
-    num[0] = num[l];
-    num[l] = t;
+    num[0] = *min;
+    // num[l] = t;
+    *min = t;
 
-    k = num[9];
-    num[9] = num[k];
-    num[k] = t;
+    t = num[9];
+    num[9] = *max; // 将最大的数*max 赋给num[9]
+    // num[k] = t;
+    *max = t; // *max所指向的地址上的数 的值变为t
 }
